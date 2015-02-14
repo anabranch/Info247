72aab0839d6fcc737c5e02674d9ae0ca4c89c2e6
anabranch <wac.chambers@gmail.com>
2015-02-13 18:39:53 -0800

    Added Average Days Version 1.0

--- cityfix/_js/main.js
+++ cityfix/_js/main.js
@@ -1,141 +1,103 @@
-// function to return color for each season
 function colors(i) {
   switch (i) {
-    case 0:
-      return "#1F77B4"; // winter
+    case "IllegalDumping":
+      return "#1F77B4";
       break;
-    case 1:
-      return "#DBDD8D"; // spring
+    case "BuildingMaintenance":
+      return "#DBDD8D";
       break;
-    case 2:
-      return "#2CA02C"; // summer
+    case "Electrical":
+      return "#2CA02C";
       break;
-    case 3:
-      return "#D62728"; // fall
+    case "StreetSweeping":
+      return "#D62728";
+      break;
+    case "RoadRepair":
+      return "#B23AEE";
+      break;
+    case "VegetationControl":
+      return "#03070b";
+      break;
+    case "Graffiti":
+      return "#ffff66";
+      break;
+    case "Survey":
+      return "#ff8d00";
+      break;
+    case "Drainage":
+      return "#00ff7f";
+      break;
+    case "StreetLights":
+      return "#01aebf";
+      break;
+
+      return "Pink";
   };
 }
-
-// set up the initial options
 var options = {
   chart: {
-    renderTo: "container",
-    type: "bar",
-    style: {
-      fontFamily: "Helvetica,Arial,sans-serif"
-    }
+    type: 'bar',
+    marginLeft:150,
+    marginRight:50
   },
-  legend: {
-    borderRadius: 0,
-    itemStyle: {
-      color: "#000000",
-      fontFamily: "Helvetica,Arial,sans-serif"
-    }
-  },
-
-  plotOptions: {
-    series: {
-      stacking: "normal" // vs. "percent"
-    }
-  },
-  series: [],
   title: {
-    text: "San Francisco Rainfall 2000-2015",
-    style: {
-      color: "#000000",
-      fontFamily: "Helvetica,Arial,sans-serif",
-      fontSize: "20px",
-      fontWeight: "bold"
+    text: 'Average Days to Respond Per Category'
+  },
+  xAxis: {
+    // title: {
+    //   text: "Report Type",
+    //   rotation:270
+    // }
+    title: {
+      align: 'high',
+      offset: 0,
+      text: 'Problem Type',
+      rotation: 0,
+      y: -10
     }
   },
   tooltip: {
-    // hints: borderRadius, borderWidth, shadow
-    borderRadius:5,
-    shadow:false,
-    borderWidth:2,
-    style: {
-      // hints: color, fontFamily, fontSize, padding
-      color:'black',
-      padding:'8px', 
-      fontSize:'13px',
+    formatter: function() {
+      return '<b>' + name + '</b><br/>' +
+        this.x + ': ' + this.y;
     }
   },
-  xAxis: {
-    categories: [],
-    labels: {
-      style: {
-        color: "#333333",
-        fontFamily: "Helvetica,Arial,sans-serif"
-      }
-    },
+  legend: {
+    enabled: false,
+    layout: 'vertical',
+    floating: true,
+    backgroundColor: '#FFFFFF',
+    align: 'right',
+    verticalAlign: 'top',
+    y: 60,
+    x: -60
   },
-  yAxis: {
-    // hints: gridLineColor, gridLineDashStyle
-    gridLineDashStyle:'dot',
-    gridLineColor:'grey',
-    labels: {
-      style: {
-        color: "#333333",
-        fontFamily: "Helvetica,Arial,sans-serif"
-      }
-    },
-    min: 0,
-    stackLabels: {
-      enabled: true,
-      style: {
-        fontFamily: "Helvetica,Arial,sans-serif",
-        fontWeight: "bold",
-        color: "#333333"
-      }
-    },
-    title: {
-      style: {
-        color: "#333333",
-        fontFamily: "Helvetica,Arial,sans-serif"
-      },
-      text: "Inches of Rain"
-    }
-  }
+  series: []
 }
 
-$.get("_data/SF_rainfall_seasons.csv", function(csv) {
-  var lines = csv.split("\n");
-  var seasons = [];
-  var rainBySeason = [
-    [],
-    [],
-    [],
-    []
-  ];
 
-  var cats = [];
+$.getJSON("_data/avgdays.json", function(jsondata) {
+  var cats = jsondata.map(function(val) {
+    return val.name;
+  });
 
-  $.each(lines, function(lineNo, line) {
-    var items = line.split(',');
+  var data = jsondata.map(function(val) {
+    return {
+      color: colors(val.name),
+      y: val.data,
+    }
+  });
 
-    if (lineNo == 0) {
-      seasons = items.slice(1, 5);
-    } else {
-      cats.push(items[0]);
-      rainBySeason[0].push(Number.parseFloat(items[1]));
-      rainBySeason[1].push(Number.parseFloat(items[2]));
-      rainBySeason[2].push(Number.parseFloat(items[3]));
-      rainBySeason[3].push(Number.parseFloat(items[4]));
-    };
 
+  var values = jsondata.map(function(val) {
+    return val.data
   });
   options.xAxis.categories = cats;
-var tempSeries = [];
-  for (var i = 0; i < rainBySeason.length; i++) {
-    tempSeries.push({
-      name:seasons[i],
-      data: rainBySeason[i],
-      color: colors(i),
-      index: rainBySeason.length -1 -i,
-      legendIndex:i
+  options.series = [{
+    data: values
+  }];
+  console.log(cats);
+  console.log(options);
 
-    });
-  };
-  options.series = tempSeries;
-console.log(tempSeries);
-  var chart = new Highcharts.Chart(options);
+  $('#daystoclose').highcharts(options);
 });
