976a2805a16cef2086afc7b727bccbdb1d62570b
anabranch <wac.chambers@gmail.com>
2015-04-03 16:09:52 -0700

    Starter version of Airplanes

--- d3-viz-assignment/js/main.js
+++ d3-viz-assignment/js/main.js
@@ -1,23 +1,7 @@
-var filename = "/data/running_data.json";
-// columns = lengths
-// rows = days
-
-var cols = [0.0, 0.7, 0.9, 1.0, 1.2, 1.3, 1.4, 1.5, 1.7, 1.8, 1.9, 2.0, 2.1, 2.2, 2.3, 2.4, 2.5, 2.9, 3.0, 3.1, 3.4, 4.0, 4.1, 4.6, 5.0, 5.2, 5.3, 5.6, 6.5, 12.0, 14.8, 26.2];
-var rows = d3.range(7);
-
-var mtx = [
-  [11, 1, 0, 0, 0, 1, 2, 2, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 2, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0],
-  [12, 0, 0, 0, 0, 0, 5, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 2, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
-  [14, 0, 0, 0, 0, 0, 2, 1, 0, 0, 0, 0, 0, 0, 0, 2, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
-  [15, 0, 2, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
-  [10, 0, 0, 0, 1, 1, 3, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 3, 1, 1, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0],
-  [13, 1, 0, 0, 0, 1, 1, 2, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0],
-  [12, 0, 0, 0, 0, 1, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-];
-
-var height = 600;
+var url = "/data/running_data.json",
+height = 600;
 var width = 600;
-var colorRange = ["#1a9850","#d73027", "#fc8d59", "#fee08b", "#ffffbf", "#d9ef8b", "#91cf60", ];
+var colorRange = ["#1a9850", "#d73027", "#fc8d59", "#fee08b", "#ffffbf", "#d9ef8b", "#91cf60", ];
 
 var fill = d3.scale.ordinal()
   .domain(d3.range(colorRange.length))
@@ -36,34 +20,36 @@ var svg = d3.select('#chart')
   .attr('height', height)
   .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-
-var layout = d3.layout.chord()
-  .padding(.04)
-  .matrix(mtx);
-
-
-svg.append("g")
-  .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")")
-  .selectAll("path")
-  .data(layout.groups)
-  .enter()
-  .append("path")
-  .style("fill", function(d, i) {
-    return fill(d.index);
-  })
-  .style("stroke", function(d) {
-    return fill(d.index);
-  })
-  .attr("d", arc);
+$.get(url, function(data) {
+      console.log(data);
+      var layout = d3.layout.chord()
+        .padding(.04)
+        .matrix(data);
 
 
-svg.append("g")
-  .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")")
-  .attr("class", "chord")
-  .selectAll("path")
-  .data(layout.chords)
-  .enter().append("path")
-  .style("fill", function(d) {
-    return fill(d.target.index);
-  })
-  .attr("d", d3.svg.chord().radius(innerRadius));
+      svg.append("g")
+        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")")
+        .selectAll("path")
+        .data(layout.groups)
+        .enter()
+        .append("path")
+        .style("fill", function(d, i) {
+          return fill(d.index);
+        })
+        .style("stroke", function(d) {
+          return fill(d.index);
+        })
+        .attr("d", arc);
+
+
+      svg.append("g")
+        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")")
+        .attr("class", "chord")
+        .selectAll("path")
+        .data(layout.chords)
+        .enter().append("path")
+        .style("fill", function(d) {
+          return fill(d.target.index);
+        })
+        .attr("d", d3.svg.chord().radius(innerRadius));
+    });
