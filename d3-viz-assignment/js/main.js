24b831ded3d64bd84ae2afdde27078ed0d6b421a
anabranch <wac.chambers@gmail.com>
2015-04-03 16:24:57 -0700

    Colorized Version of Airplanes, no animation or labels

--- d3-viz-assignment/js/main.js
+++ d3-viz-assignment/js/main.js
@@ -1,11 +1,14 @@
-var url = "/data/running_data.json",
-height = 600;
+var url = "/data/matrix.json",
+  height = 600;
 var width = 600;
-var colorRange = ["#1a9850", "#d73027", "#fc8d59", "#fee08b", "#ffffbf", "#d9ef8b", "#91cf60", ];
 
+// Colors Can be substituted to preference, in many ways less colors makes it nice...
+var colorRange = ["#1a9850", "#d73027", "#fc8d59", "#fee08b", "#ffffbf", "#d9ef8b", "#91cf60"];
 var fill = d3.scale.ordinal()
   .domain(d3.range(colorRange.length))
   .range(colorRange);
+
+var fill = d3.scale.category20b();
 
 var outerRadius = Math.min(width, height) / 2 - 8,
   innerRadius = outerRadius - 30;
@@ -20,36 +23,37 @@ var svg = d3.select('#chart')
   .attr('height', height)
   .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-$.get(url, function(data) {
-      console.log(data);
-      var layout = d3.layout.chord()
-        .padding(.04)
-        .matrix(data);
+$.getJSON(url, function(data) {
 
 
-      svg.append("g")
-        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")")
-        .selectAll("path")
-        .data(layout.groups)
-        .enter()
-        .append("path")
-        .style("fill", function(d, i) {
-          return fill(d.index);
-        })
-        .style("stroke", function(d) {
-          return fill(d.index);
-        })
-        .attr("d", arc);
+  var layout = d3.layout.chord()
+    .padding(.04)
+    .matrix(data);
 
 
-      svg.append("g")
-        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")")
-        .attr("class", "chord")
-        .selectAll("path")
-        .data(layout.chords)
-        .enter().append("path")
-        .style("fill", function(d) {
-          return fill(d.target.index);
-        })
-        .attr("d", d3.svg.chord().radius(innerRadius));
-    });
+  svg.append("g")
+    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")")
+    .selectAll("path")
+    .data(layout.groups)
+    .enter()
+    .append("path")
+    .style("fill", function(d, i) {
+      return fill(d.index);
+    })
+    .style("stroke", function(d) {
+      return fill(d.index);
+    })
+    .attr("d", arc);
+
+
+  svg.append("g")
+    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")")
+    .attr("class", "chord")
+    .selectAll("path")
+    .data(layout.chords)
+    .enter().append("path")
+    .style("fill", function(d) {
+      return fill(d.target.index);
+    })
+    .attr("d", d3.svg.chord().radius(innerRadius));
+});
